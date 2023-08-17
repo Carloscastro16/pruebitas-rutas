d0f5810b0be8b8c16195beb0b2204c1055b27892
ccastro1016 <andycastro.001016@gmail.com>
2023-08-16 21:49:50 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pruebitas-rutas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
